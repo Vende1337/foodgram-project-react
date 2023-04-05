de2cb1b3bc2e252fc10c1c1f59b07a279e5dd312
Дмитрий Венде <vende.d@mail.ru>
2023-04-05 13:58:43 +0300

    z

--- backend/users/admin.py
+++ backend/users/admin.py
@@ -5,20 +5,20 @@ from django.contrib.auth.forms import UserCreationForm
 from .models import User
 
 
-class CustomUserCreationForm(UserCreationForm):
-    class Meta:
-        model = User
-        fields = ('username', 'email', 'first_name', 'last_name',)
+# class CustomUserCreationForm(UserCreationForm):
+#     class Meta:
+#         model = User
+#         fields = ('username', 'email', 'first_name', 'last_name',)
 
 
 @register(User)
-class CustomUserAdmin(UserAdmin):
-    add_form = CustomUserCreationForm
-    add_fieldsets = (
-        (None, {
-            'classes': ('wide',),
-            'fields': ('username', 'email', 'first_name', 'last_name',),
-        }),
-    )
+class CustomUserAdmin(admin.ModelAdmin):
+    # add_form = CustomUserCreationForm
+    # add_fieldsets = (
+    #     (None, {
+    #         'classes': ('wide',),
+    #         'fields': ('username', 'email', 'first_name', 'last_name',),
+    #     }),
+    # )
     list_display = ('username', 'email', 'first_name', 'last_name')
     list_filter = ('username', 'email')
